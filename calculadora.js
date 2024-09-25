08a898770a9ecf03222ec53aac4bc3a817f55c3a
sebasjimenez <sebasjimenez1121@gamil.com>
2024-09-25 15:16:38 -0500

    finalizacion de la calcudora

--- calculadora.js
+++ calculadora.js
@@ -2,54 +2,45 @@ const btn = document.querySelectorAll('.btn');
 let valorActual = document.querySelector('.pantalla');
 let valorAnteriror = "";
 let pocision = 0;
+let operacion = 0;
 let operadores = [];
 let operandos = [];
 
-/* switch (operador) {
-    case '+':
-        valorAnteriror = valorActual.value;
-        operadores.push(valorAnteriror);
-        valorActual.value = valorAnteriror + "+"
-        primeraOperacion = 0;
-        break;
-    case '-':
-        valorAnteriror = valorActual.value;
-        operaciones.push(valorAnteriror);
-        valorActual.value = valorAnteriror + "-"
-        primeraOperacion = 0;
-        break;
-    case '*':
-        valorAnteriror = valorActual.value;
-        operaciones.push(valorAnteriror);
-        valorActual.value = valorAnteriror + "*"
-        primeraOperacion = 0;
-        break;
-    case '/':
-        valorAnteriror = valorActual.value;
-        operaciones.push(valorAnteriror);
-        valorActual.value = valorAnteriror + "/"
-        primeraOperacion = 0;
-        break;
-    default:
-        break;
-} */
 btn.forEach((btn) => {
     btn.addEventListener('click', () => {
         switch (btn.id) {
             case 'suma':
-                
+                operandos.push(parseFloat(valorOperando()))
+                operadores.push("+");
+                valorActual.value = valorActual.value + '+';
+                valorAnteriror = valorActual.value;
                 break;
             case 'resta':
-
+                operandos.push(parseFloat(valorOperando()))
+                operadores.push("-");
+                valorActual.value = valorActual.value + '-';
+                valorAnteriror = valorActual.value;
                 break;
             case 'division':
-
+                operandos.push(parseFloat(valorOperando()))
+                operadores.push("/");
+                valorActual.value = valorActual.value + '/';
+                valorAnteriror = valorActual.value;
                 break;
             case 'multiplicacion':
-
+                operandos.push(parseFloat(valorOperando()))
+                operadores.push("*");
+                valorActual.value = valorActual.value + '*';
+                valorAnteriror = valorActual.value;
                 break;
             case 'total':
-
+                operandos.push(parseFloat(valorOperando()));
+                const totalOperacion = total();
+                valorActual.value = totalOperacion;
+                operadores = [];
+                operandos = [];
+                valorAnteriror = valorActual.value;
+                operacion = 0;
                 break;
             case 'borrar':
 
@@ -67,6 +58,60 @@ const agregar = (content) => {
     } else {
         valorActual.value = valorActual.value + content;
     }
+}
+
+const valorOperando = () => {
+    if (operacion === 0) {
+        valorAnteriror = valorActual.value;
+        operacion = 1;
+        return valorActual.value;
+    } else {
+        let valor = "";
+        for (let i = 0; i < valorActual.value.length; i++) {
+            if (valorActual.value[i] != valorAnteriror[i]) {
+                valor += valorActual.value[i];
+            }
+        }
+        return valor;
+    }
+}
+
+const total = () => {
+    let ops = [...operadores];
+    let nums = [...operandos];
+    
+    for (let i = 0; i < ops.length; i++) {
+        if (ops[i] === "*" || ops[i] === "/") {
+            let result;
+            if (ops[i] === "*") {
+                result = multiplicacion(nums[i], nums[i + 1]);
+            } else if (ops[i] === "/") {
+                result = division(nums[i], nums[i + 1]);
+            }
+            nums[i] = result;
+            nums.splice(i + 1, 1);
+            ops.splice(i, 1);
+            i--;
+        }
+    }
+
+    for (let i = 0; i < ops.length; i++) {
+        let result;
+        if (ops[i] === "+") {
+            result = suma(nums[i], nums[i + 1]);
+        } else if (ops[i] === "-") {
+            result = resta(nums[i], nums[i + 1]);
+        }
+
+        nums[i] = result;
+        nums.splice(i + 1, 1);
+        ops.splice(i, 1);
+        i--;
+    }
+
+    
+    let totalOperacion = nums[0];
+    return totalOperacion;
 }
 
 const suma = (num1, num2) => {
