3cec94549f04f4b0e9516505655a08ece1c199c3
sebasjimenez <sebasjimenez1121@gamil.com>
2024-09-26 09:24:36 -0500

    actualizacion del scrpt php

--- calculadora.php
+++ calculadora.php
@@ -6,7 +6,6 @@ $calculadora = new Calculadora;
 
 
 if (isset($_GET['OP'])) {
-    if ($_GET['OP'] == 'suma') {
-        print $calculadora->suma(1, 3);
-    }
+
+    print $calculadora->$_GET['OP'](5, 3);
 }
