cef4d970bb230bc9d807e9a190c3a4632c93206e
sebasjimenez <sebasjimenez1121@gamil.com>
2024-09-26 08:57:40 -0500

    actualizacion fetch

--- calculadora.js
+++ calculadora.js
@@ -14,7 +14,19 @@ document.addEventListener('keydown', (e) => {
 
     switch (e.key) {
         case "+":
-            fetch('/calculadora.php?OP=suma')
+            fetch('/calculadora.php?OP=suma', {
+                method: 'GET', // *GET, POST, PUT, DELETE, etc.
+                mode: 'cors', // no-cors, *cors, same-origin
+                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
+                    'Content-Type': 'text/html'
+                    // 'Content-Type': 'application/x-www-form-urlencoded',
+                },
+                redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+            })
                 .then(response => response.json()).
                 then(data => {
                     console.log(data);
